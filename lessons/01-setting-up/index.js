b23d8d01f435ed00b828457e8c72f27a11c7a480
Seiji Naganuma <snaganumas@gmail.com>
2016-07-03 15:49:30 -0400

    go through tutorial

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,27 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
+import Repo from './modules/Repo'
+import Home from './modules/Home'
+
+//import react router
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
+//render router and define route-component
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      {/* Index is only used when route matches exactly */}
+      <IndexRoute component={Home}/>
+
+      {/* make them children of `App` */}
+      <Route path="/repos" component={Repos}>
+        {/* add the new route with params */}
+        <Route path="/repos/:userName/:repoName" component={Repo}/>
+      </Route>
+      <Route path="/about" component={About}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
--- lessons/01-setting-up/webpack.config.js
+++ lessons/01-setting-up/webpack.config.js
@@ -1,10 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: './index.js',
 
   output: {
     filename: 'bundle.js',
-    publicPath: ''
+    path: 'public'
   },
+
+  // add this handful of plugins that optimize the build
+  // when we're in production
+  plugins: process.env.NODE_ENV === 'production' ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ] : [],
 
   module: {
     loaders: [
